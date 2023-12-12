f061213b12ae8cfc17e11d1f4d4fa684ce9d3e02
sungwoo <swpheus1@gmail.com>
2023-12-12 12:36:24 +0900

    충돌 수정

--- DAY5/express-nodesample1/public/common/sample.js
+++ DAY5/express-nodesample1/public/common/sample.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function fnValidation() {
   //만약 html 문서의 email 아이디가 있다면
 
@@ -19,20 +18,3 @@ function fnValidation() {
     return false;
   }
 }
-=======
-// 사용자 입력값 유효성 체크함수
-function fnValidation() {
-
-    if(document.getElementById("email").value == "") {
-        alert("메일주소 아이디를 입력해주세요.");
-        document.getElementById("email").focus();
-        return false;
-    }
-    
-    if(document.getElementById("password").value == "") {
-        alert("사용자 암호를 입력해주세요.");
-        document.getElementById("password").focus();
-        return false;
-    }
-}
->>>>>>> cca7292 (Day10)
